6a171d9fd918abb0b366fe581e75a5ad458d7e7e
AlekseyMar <alekseyamart@gmail.com>
2019-12-11 01:49:01 +0200

    вывел список меню

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './component/navbar/home/home.component';
+import { SubdivisionComponent } from './component/navbar/subdivision/subdivision.component';
+import { GalleryComponent } from './component/navbar/gallery/gallery.component';
+import { AboutUsComponent } from './component/navbar/about-us/about-us.component';
+import { RegistryComponent } from './component/navbar/registry/registry.component';
+import { InputComponent } from './component/navbar/input/input.component';
+import { OutputComponent } from './component/navbar/output/output.component';
+import { NotFoundComponent } from './component/navbar/not-found/not-found.component';
+import { NewsComponent } from './component/navbar/news/news.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'subdivision', component: SubdivisionComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'registry', component: RegistryComponent },
+  { path: 'input', component: InputComponent },
+  { path: 'output', component: OutputComponent },
+  { path: 'news', component: NewsComponent },
+  { path: '**', component: NotFoundComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    SubdivisionComponent,
+    GalleryComponent,
+    AboutUsComponent,
+    RegistryComponent,
+    InputComponent,
+    OutputComponent,
+    NotFoundComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
